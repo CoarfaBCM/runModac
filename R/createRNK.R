08befcfc0395fc27f9384ce38922ac5a81e85ecc
trgandhi <trgandhi1@gmail.com>
2023-02-06 13:12:30 -0600

    rnk files fixed

--- R/createRNK.R
+++ R/createRNK.R
@@ -3,5 +3,5 @@ createRNK <- function(reportFile, outFile) {
   data <- read.csv(reportFile, check.names = F)
   data <- data[order(data[,grepl("fc",colnames(data))], decreasing = T),]
   
-  write.table(data[,c(1, which(grepl("fc",colnames(data))))], outFile)
+  write.table(data[,c(1, which(grepl("fc",colnames(data))))], outFile, quote = F, sep = "\t", row.names = F)
 }
