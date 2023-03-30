216254321c9335dce5419e663bfe86c68be46757
trgandhi <trgandhi1@gmail.com>
2023-03-29 20:46:51 -0500

    Updated analysis.R with absolute paths

--- analysis.R
+++ analysis.R
@@ -1,57 +1,57 @@
-source("R/runModac.R")
+source("~/Box Sync/runModac/R/runModac.R")
 
 # example1-metabolomics
-runModac(inputFile = "example1-metabolomics/input/input-A.xlsx",
-         comparisonsFile = "example1-metabolomics/input/comparisons-A.xlsx",
+runModac(inputFile = "~/Box Sync/runModac/example1-metabolomics/input/input-A.xlsx",
+         comparisonsFile = "~/Box Sync/runModac/example1-metabolomics/input/comparisons-A.xlsx",
          type = "metabolomics",
-         outdir = "example1-metabolomics/results-A",
+         outdir = "~/Box Sync/runModac/example1-metabolomics/results-A",
          project_title = "Project Report",
          project_subtitle = "Metabolomics Analysis",
          scriptPath = "~/Box Sync/runModac/R/")
 
 # example2-prenormalized-dummy
-runModac(inputFile = "example2-prenormalized-dummy/input/input.xlsx",
-         comparisonsFile = "example2-prenormalized-dummy/input/comparisons.xlsx",
+runModac(inputFile = "~/Box Sync/runModac/example2-prenormalized-dummy/input/input.xlsx",
+         comparisonsFile = "~/Box Sync/runModac/example2-prenormalized-dummy/input/comparisons.xlsx",
          type = "metabolomics",
-         outdir = "example2-prenormalized-dummy/results",
+         outdir = "~/Box Sync/runModac/example2-prenormalized-dummy/results",
          project_title = "Project Report",
          project_subtitle = "Metabolomics Analysis [prenormalized w/ dummy QC]",
          scriptPath = "~/Box Sync/runModac/R/")
 
 # example3-iqr
-runModac(inputFile = "example3-iqr/input/input.xlsx",
-         comparisonsFile = "example3-iqr/input/comparisons.xlsx",
+runModac(inputFile = "~/Box Sync/runModac/example3-iqr/input/input.xlsx",
+         comparisonsFile = "~/Box Sync/runModac/example3-iqr/input/comparisons.xlsx",
          type = "metabolomics",
-         outdir = "example3-iqr/results",
+         outdir = "~/Box Sync/runModac/example3-iqr/results",
          project_title = "Project Report",
          project_subtitle = "Metabolomics Analysis [IQR]",
          scriptPath = "~/Box Sync/runModac/R/",
          heatmap_color_scale = c("blue","white","red"))
 
 # example4-prenormalized-nodummy
-runModac(inputFile = "example4-prenormalized-nodummy/input/input.xlsx",
-         comparisonsFile = "example4-prenormalized-nodummy/input/comparisons.xlsx",
+runModac(inputFile = "~/Box Sync/runModac/example4-prenormalized-nodummy/input/input.xlsx",
+         comparisonsFile = "~/Box Sync/runModac/example4-prenormalized-nodummy/input/comparisons.xlsx",
          type = "metabolomics",
-         outdir = "example4-prenormalized-nodummy/results",
+         outdir = "~/Box Sync/runModac/example4-prenormalized-nodummy/results",
          project_title = "Project Report",
          project_subtitle = "Metabolomics Analysis [prenormalized]",
          scriptPath = "~/Box Sync/runModac/R/")
 
 # example5-transposed
-runModac(inputFile = "example5-transposed/input/input.xlsx",
-         comparisonsFile = "example5-transposed/input/comparisons.xlsx",
+runModac(inputFile = "~/Box Sync/runModac/example5-transposed/input/input.xlsx",
+         comparisonsFile = "~/Box Sync/runModac/example5-transposed/input/comparisons.xlsx",
          type = "metabolomics",
          samplesAreRows = F,
-         outdir = "example5-transposed/results",
+         outdir = "~/Box Sync/runModac/example5-transposed/results",
          project_title = "Project Report",
          project_subtitle = "Example w/ Transposed Data",
          scriptPath = "~/Box Sync/runModac/R/")
 
 # example6-rppa
-runModac(inputFile = "example6-rppa/input/input.xlsx",
-         comparisonsFile = "example6-rppa/input/comparisons.xlsx",
+runModac(inputFile = "~/Box Sync/runModac/example6-rppa/input/input.xlsx",
+         comparisonsFile = "~/Box Sync/runModac/example6-rppa/input/comparisons.xlsx",
          type = "rppa",
-         outdir = "example6-rppa/results",
+         outdir = "~/Box Sync/runModac/example6-rppa/results",
          samplesAreRows = F,
          sampleIDRow = 1,
          project_title = "Project Report",
