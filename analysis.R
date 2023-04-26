a5723e15c0b13a244e64cab418a7182612ebbfc4
trgandhi <trgandhi1@gmail.com>
2023-04-26 11:05:03 -0500

    added intro to modac slides
    added intro to modac slides and updated analysis.R with the heatmap_color_scale option commented out in every example function call so users know it's available. closes #35

--- analysis.R
+++ analysis.R
@@ -7,6 +7,7 @@ runModac(inputFile = "~/Box Sync/runModac/example1-metabolomics/input/input-A.xl
          outdir = "~/Box Sync/runModac/example1-metabolomics/results-A",
          project_title = "Project Report",
          project_subtitle = "Metabolomics Analysis",
+         # heatmap_color_scale = c("blue","white","red"),
          scriptPath = "~/Box Sync/runModac/R/")
 
 # example2-prenormalized-dummy
@@ -16,6 +17,7 @@ runModac(inputFile = "~/Box Sync/runModac/example2-prenormalized-dummy/input/inp
          outdir = "~/Box Sync/runModac/example2-prenormalized-dummy/results",
          project_title = "Project Report",
          project_subtitle = "Metabolomics Analysis [prenormalized w/ dummy QC]",
+         # heatmap_color_scale = c("blue","white","red"),
          scriptPath = "~/Box Sync/runModac/R/")
 
 # example3-iqr
@@ -25,8 +27,8 @@ runModac(inputFile = "~/Box Sync/runModac/example3-iqr/input/input.xlsx",
          outdir = "~/Box Sync/runModac/example3-iqr/results",
          project_title = "Project Report",
          project_subtitle = "Metabolomics Analysis [IQR]",
-         scriptPath = "~/Box Sync/runModac/R/",
-         heatmap_color_scale = c("blue","white","red"))
+         heatmap_color_scale = c("blue","white","red"),
+         scriptPath = "~/Box Sync/runModac/R/")
 
 # example4-prenormalized-nodummy
 runModac(inputFile = "~/Box Sync/runModac/example4-prenormalized-nodummy/input/input.xlsx",
@@ -35,6 +37,7 @@ runModac(inputFile = "~/Box Sync/runModac/example4-prenormalized-nodummy/input/i
          outdir = "~/Box Sync/runModac/example4-prenormalized-nodummy/results",
          project_title = "Project Report",
          project_subtitle = "Metabolomics Analysis [prenormalized]",
+         # heatmap_color_scale = c("blue","white","red"),
          scriptPath = "~/Box Sync/runModac/R/")
 
 # example5-transposed
@@ -45,6 +48,7 @@ runModac(inputFile = "~/Box Sync/runModac/example5-transposed/input/input.xlsx",
          outdir = "~/Box Sync/runModac/example5-transposed/results",
          project_title = "Project Report",
          project_subtitle = "Example w/ Transposed Data",
+         # heatmap_color_scale = c("blue","white","red"),
          scriptPath = "~/Box Sync/runModac/R/")
 
 # example6-rppa
@@ -56,4 +60,5 @@ runModac(inputFile = "~/Box Sync/runModac/example6-rppa/input/input.xlsx",
          sampleIDRow = 1,
          project_title = "Project Report",
          project_subtitle = "RPPA Analysis",
+         # heatmap_color_scale = c("blue","white","red"),
          scriptPath = "~/Box Sync/runModac/R/")
